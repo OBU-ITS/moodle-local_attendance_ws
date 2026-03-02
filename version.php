922a91d609a35d284a82d0ff224a81e16f66aaca
Emir Kamel <p0092396@brookes.ac.uk>
2026-03-02 16:14:11 +0000

    updating version

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_attendance_ws';
-$plugin->version = 2026021600;
+$plugin->version = 2026030200;
 $plugin->requires = 2012120301;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v1.7.0.2';
+$plugin->release = 'v1.7.0.3';
 $plugin->dependencies = array(
     'mod_attendance' => 2024070301, // OBU Customisation fork
     'local_obu_metalinking' => 2024110101,
