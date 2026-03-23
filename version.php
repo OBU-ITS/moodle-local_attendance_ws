1c8e7ae06dfcc438e3ac740db47945c5fe621eba
Emir Kamel <p0092396@brookes.ac.uk>
2026-03-23 15:17:56 +0000

    version bump to introduce sched task to moodle

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_attendance_ws';
-$plugin->version = 2026032301;
+$plugin->version = 2026032302;
 $plugin->requires = 2012120301;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v1.7.0.6';
+$plugin->release = 'v1.7.0.7';
 $plugin->dependencies = array(
     'mod_attendance' => 2024070301, // OBU Customisation fork
     'local_obu_metalinking' => 2024110101,
