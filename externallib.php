005b455f727fbb77ba2fd905f173fca48309570a
Emir Kamel <p0092396@brookes.ac.uk>
2026-03-23 13:43:42 +0000

    incorporating use of locallib in current live code in externallib to reduce duplicate code

--- externallib.php
+++ externallib.php
@@ -78,99 +78,7 @@ class local_attendance_ws_external extends external_api {
 			return array('result' => -1);
 		}
 
-		if (!($course = $DB->get_record('course', array('idnumber' => $params['idnumber'])))) {
-			return array('result' => -2);
-		}
-
-        $teachingcourse = local_obu_metalinking_get_teaching_course($course);
-        if (!($attendance = local_attendance_ws_find_attendance_activity($teachingcourse))) {
-            return array('result' => -3);
-        }
-
-		if (!($cm = get_coursemodule_from_instance('attendance', $attendance->id, 0, false))) {
-			return array('result' => -4);
-		}
-
-        $pluginconfig = get_config('attendance');
-
-		// Capability checking
-		$context = context_module::instance($cm->id);
-		require_capability('mod/attendance:manageattendances', $context);
-
-		$session = new stdClass();
-		$session->attendanceid = $attendance->id;
-        $session->timetableeventid = $params['slotid'];
-        $session->roomid = $params['roomid'];
-		$session->sessdate = $params['start'];
-		$session->duration = $params['duration'];
-		$session->lasttaken = null;
-		$session->lasttakenby = 0;
-		$session->timemodified = time();
-
-        $usergroup = ($params['group'] == '0' || $params['group'] == '')
-            ? local_obu_group_manager_create_system_group($course, null, null, null, null, $teachingcourse)
-            : local_obu_group_manager_create_system_group($course, null, null, $semesterName, $group, $teachingcourse);
-
-        $session->groupid = $usergroup->id;
-
-        $session->description = "Room(s): " . $params['roomid'];
-
- 		$session->descriptionformat = 1;
-		$session->statusset = 0;
-        $session->calendarevent = 0;
-
-        $salt = get_config('local_attendance_ws', 'salt');
-        $session->studentpassword = local_attendance_ws_password_hash($params['slotid'], $params['roomid'], $params['start'], 6, $salt);
-        $session->sessioninstancecode = local_attendance_ws_session_instance_code($params['slotid'], $params['roomid'], $params['start']);
-
-        if (isset($pluginconfig->calendarevent_default)) {
-            $session->caleventid = $pluginconfig->calendarevent_default;
-        }
-        if (isset($pluginconfig->studentscanmark_default)) {
-            $session->studentscanmark = $pluginconfig->studentscanmark_default;
-        }
-        if (isset($pluginconfig->randompassword_default)) {
-            $session->randompassword = $pluginconfig->randompassword_default;
-        }
-        if (isset($pluginconfig->includeqrcode_default)) {
-            $session->includeqrcode = $pluginconfig->includeqrcode_default;
-        }
-        if (isset($pluginconfig->autoassignstatus)) {
-            $session->autoassignstatus = $pluginconfig->autoassignstatus;
-        }
-        if (isset($pluginconfig->allowupdatestatus_default)) {
-            $session->allowupdatestatus = $pluginconfig->allowupdatestatus_default;
-        }
-        if (isset($pluginconfig->rotateqrcode_default)) {
-            $session->rotateqrcode = $pluginconfig->rotateqrcode_default;
-        }
-        if (isset($pluginconfig->automark_default)) {
-            $session->automark = $pluginconfig->automark_default;
-        }
-        if (isset($pluginconfig->studentsearlyopentime)) {
-            $session->studentsearlyopentime = $pluginconfig->studentsearlyopentime;
-        }
-        if (!empty($session->rotateqrcode)) {
-            $session->studentpassword = local_attendance_ws_password_hash($params['slotid'], $params['roomid'], $params['start'], 6, $salt);
-            $session->rotateqrcodesecret = local_attendance_ws_password_hash($params['slotid'], $params['roomid'], $params['start'], 6, $salt);
-        }
-
-		$session->id = $DB->insert_record('attendance_sessions', $session);
-		attendance_create_calendar_event($session);
-
-		// Trigger a session added event
-		$event = \mod_attendance\event\session_added::create(array(
-			'objectid' => $attendance->id,
-			'context' => $context,
-			'other' => array('info' => construct_session_full_date_time($session->sessdate, $session->duration))
-		));
-		$event->add_record_snapshot('course_modules', $cm);
-		$event->add_record_snapshot('attendance_sessions', $session);
-		$event->trigger();
-
-		mod_attendance_notifyqueue::notify_success(get_string('sessiongenerated', 'attendance'));
-
-		return array('result' => $session->id);
+		return local_attendance_ws_create_session($params['idnumber'], $params['slotid'], $params['roomid'], $params['group'], $params['start'], $params['duration'], $params['semesterName']);
 	}
 
     // DEPRECATED - remove after implementation of Upsert
@@ -210,39 +118,7 @@ class local_attendance_ws_external extends external_api {
 			return array('result' => -1);
 		}
 
-		if (!($session = $DB->get_record('attendance_sessions', array('id' => $params['sessionid'])))) {
-			return array('result' => 0);
-		}
-
-		if (!($cm = get_coursemodule_from_instance('attendance', $session->attendanceid, 0, false))) {
-			return array('result' => -2);
-		}
-
-		// Capability checking
-		$context = context_module::instance($cm->id);
-		require_capability('mod/attendance:manageattendances', $context);
-
-		$session->sessdate = $params['start'];
-        $session->duration = $params['duration'];
-        $session->roomid = $params['roomid'];
-        $session->description = "Room(s): " . $params['roomid'];
-		$session->timemodified = time();
-		$DB->update_record('attendance_sessions', $session);
-
-		$event = \mod_attendance\event\session_updated::create(array(
-			'objectid' => $session->attendanceid,
-			'context' => $context,
-			'other' => array(
-				'info' => construct_session_full_date_time($session->sessdate, $session->duration),
-				'sessionid' => $session->id,
-				'action' => mod_attendance_sessions_page_params::ACTION_UPDATE
-			)
-		));
-        $event->add_record_snapshot('course_modules', $cm);
-        $event->add_record_snapshot('attendance_sessions', $session);
-        $event->trigger();
-
-		return array('result' => $params['sessionid']);
+		return local_attendance_ws_update_session($params['sessionid'], $params['start'], $params['duration'], $params['roomid']);
 	}
 
     // DEPRECATED - remove after full implementation of Upsert
@@ -276,36 +152,9 @@ class local_attendance_ws_external extends external_api {
 			return array('result' => -1);
 		}
 
-		if (!($session = $DB->get_record('attendance_sessions', array('id' => $params['sessionid'])))) {
-			return array('result' => 0);
-		}
-
-		if (!($cm = get_coursemodule_from_instance('attendance', $session->attendanceid, 0, false))) {
-			return array('result' => -2);
-		}
-
-		// Capability checking
-		$context = context_module::instance($cm->id);
-		require_capability('mod/attendance:manageattendances', $context);
-
-		if ($session->caleventid) {
-			attendance_delete_calendar_events(array($params['sessionid']));
-		}
-
-		$DB->delete_records('attendance_log', array('sessionid' => $params['sessionid']));
-		$DB->delete_records('attendance_sessions', array('id' => $params['sessionid']));
-		$event = \mod_attendance\event\session_deleted::create(array(
-			'objectid' => $session->attendanceid,
-			'context' => $context,
-			'other' => array('info' => $params['sessionid'])
-		));
-        $event->add_record_snapshot('course_modules', $cm);
-        $event->trigger();
-
-		return array('result' => $params['sessionid']);
+		return local_attendance_ws_delete_session($params['sessionid']);
 	}
 
-    //TODO :: Upsert functionality
     public static function upsert_sessions_parameters() {
         return new external_function_parameters(
             array(
